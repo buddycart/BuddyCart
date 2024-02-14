617e8dbc5294808c4642ecb5fcaa389106b6c967
Émilie P <emiliep.dev@gmail.com>
2024-02-14 15:44:07 +0100

    First commit

---
+++ app/src/main/java/com/buddycart/MainApplication.kt
@@ -0,0 +1,6 @@
+package com.buddycart
+
+import android.app.Application
+
+class MainApplication : Application() {
+}
---
+++ app/src/main/java/com/buddycart/buddycart/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.buddycart.buddycart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.buddycart.buddycart.ui.theme.BuddyCartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BuddyCartTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BuddyCartTheme {
+        Greeting("Android")
+    }
+}
